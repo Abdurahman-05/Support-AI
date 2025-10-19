eddca16d142a537f8739bd6fdf7789c3985ad821
Abdurahman <eabdurahman8@gmail.com>
2025-10-19 21:31:16 +0300

    supabase auth

--- my-app/lib/supabase/server.ts
+++ my-app/lib/supabase/server.ts
@@ -1,12 +1,29 @@
 import { createServerClient } from '@supabase/ssr'
-import type { SupabaseClient } from '@supabase/supabase-js'
 import { cookies } from 'next/headers'
 
-export function createServerClientHelper(): SupabaseClient {
-  const cookieStore = cookies()
+export async function createClient() {
+  const cookieStore = await cookies()
+
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
-    { cookies: cookieStore }
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
   )
 }
